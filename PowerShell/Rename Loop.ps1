25e76e5988fc6906ba41e164a626784434fdec85
slam1987 <45326998+slam1987@users.noreply.github.com>
2019-07-31 18:01:02 -0700

    Add files via upload

---
+++ PowerShell/Rename Loop.ps1
@@ -0,0 +1,12 @@
+﻿# The purpose of this script was to rename a bunch of video files due to naming compatibility issues with Plex Media Server
+
+# Variables
+$directoryPath = "E:\Television Shows\The Golden Girls\"
+$folderList = Get-ChildItem $directoryPath -Directory
+$counter = 0
+
+# For loop to rename the files using a counter
+ForEach($folderItem in $folderList) {
+$counter = $counter + 1
+Get-ChildItem $directoryPath\$folderItem\ | Rename-Item -NewName {$_.Name.Replace("The Golden Girls 0$counter-","") }
+}
